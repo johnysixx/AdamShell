9c352a80853bcc2c07dcb59482b79daaa5a43e63
johny <johnysixxvega@gmail.com>
2026-07-16 21:01:59 +0200

    Add pre cosmic entropy reservoir rules

--- universe/pre_cosmic_rules.py
+++ universe/pre_cosmic_rules.py
@@ -11,7 +11,18 @@ ENERGY_SERVING_J = PLANCK_ENERGY_THRESHOLD_J * ENERGY_SERVING_RATIO
 REALITY_PAYMENT_RATIO = 0.01
 WILL_GAIN_PER_ENERGY_SERVING = 0.2
 
-SERPENT_ENTROPY_DRINK_ENERGY_GAIN_J = ENERGY_SERVING_J / IDEA_DIMENSIONS
+ENTROPY_UNIT = 1.0
+QUANTUM_ENTROPY_TICK_MAX_UNITS = 1 / IDEA_DIMENSIONS
+QUANTUM_ENTROPY_TICK_EXPECTED_UNITS = QUANTUM_ENTROPY_TICK_MAX_UNITS / 2
+EXPECTED_QUANTUM_TICKS_PER_ENTROPY_SERVING = (
+    ENTROPY_UNIT / QUANTUM_ENTROPY_TICK_EXPECTED_UNITS
+)
+
+ENTROPY_UNIT_ENERGY_VALUE_J = ENERGY_SERVING_J / IDEA_DIMENSIONS
+SERPENT_ENTROPY_DRINK_UNITS = ENTROPY_UNIT
+SERPENT_ENTROPY_DRINK_ENERGY_GAIN_J = (
+    SERPENT_ENTROPY_DRINK_UNITS * ENTROPY_UNIT_ENERGY_VALUE_J
+)
 SERPENT_ENTROPY_PAYMENT_RATIO = 0.0
 SERPENT_ENTROPY_INCREASE_RATIO = 0.0
 
@@ -55,14 +66,24 @@ def describe_pre_cosmic_energy_budget():
         "energy_serving_j": ENERGY_SERVING_J,
         "reality_payment_ratio": REALITY_PAYMENT_RATIO,
         "will_gain_per_energy_serving": WILL_GAIN_PER_ENERGY_SERVING,
+
+        "entropy_unit": ENTROPY_UNIT,
+        "quantum_entropy_tick_max_units": QUANTUM_ENTROPY_TICK_MAX_UNITS,
+        "quantum_entropy_tick_expected_units": QUANTUM_ENTROPY_TICK_EXPECTED_UNITS,
+        "expected_quantum_ticks_per_entropy_serving": EXPECTED_QUANTUM_TICKS_PER_ENTROPY_SERVING,
+        "entropy_unit_energy_value_j": ENTROPY_UNIT_ENERGY_VALUE_J,
+
+        "serpent_entropy_drink_units": SERPENT_ENTROPY_DRINK_UNITS,
         "serpent_entropy_drink_energy_gain_j": serpent_entropy_energy_gain,
         "serpent_entropy_payment_ratio": SERPENT_ENTROPY_PAYMENT_RATIO,
         "serpent_entropy_increase_ratio": SERPENT_ENTROPY_INCREASE_RATIO,
+
         "reservoir_before_serving_j": reservoir_before_serving,
         "reservoir_before_serving_ratio": reservoir_before_serving / PLANCK_ENERGY_THRESHOLD_J,
         "first_energy_servings_total_j": first_energy_servings_total,
         "reservoir_after_first_energy_servings_j": reservoir_after_first_energy_servings,
         "reservoir_after_first_energy_servings_ratio": reservoir_after_first_energy_servings / PLANCK_ENERGY_THRESHOLD_J,
+
         "god_entropy_gain_ratio": GOD_ENTROPY_GAIN_RATIO,
         "normal_entropy_gain_ratio": NORMAL_ENTROPY_GAIN_RATIO,
     }
